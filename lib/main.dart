05f390c6f6f1bd3a6fe0fc9d3dfb3abc4d44aca9
HamzaMihfad <mihfad1hamza@gmail.com>
2020-08-08 17:40:00 +0100

    first push

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            // image: NetworkImage(
+            //   'https://www.w3schools.com/w3css/img_lights.jpg',
+            // ),
+            image: AssetImage(
+              'images/diamond.png',
+            ),
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+    ),
+  );
+}
